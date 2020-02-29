23196430b75503cca76d3c18d184e76f0b7a8d0c
Ashleigh <ashleighaathar@gmail.com>
2020-02-29 20:09:52 +0800

    add session to store person's name in a session

--- app.py
+++ app.py
@@ -1,22 +1,30 @@
-from flask import Flask, render_template, request, redirect, url_for
+import uuid
+
+from flask import Flask, render_template, request, redirect, url_for, session
 
 app = Flask(__name__, static_folder="/templates/static")
-
+app.secret_key = uuid.uuid4().hex
 
 @app.route('/', methods=['GET', 'POST'])
 def greetings():
-    if request.method == 'POST':
-        pesteree = request.form['name']
-        return redirect(url_for('pester'), vars=pesteree)
-    return render_template("index.html")
+    if request.method == 'POST': # if user has submitted their name
+        person = request.form['name']
+        session['person'] = person
+        return redirect(url_for('express_disappointment')) # redirect them to the url to express disappointment
+    else:
+        return render_template('index.html') # else just return the main welcome message
 
 @app.route('/test') # to test the base.html file
 def test():
     return render_template("base.html")
 
-@app.route('/<pesteree>', methods=['GET', 'POST'])
-def main_pestering(pesteree):
-    return render_template("pester.html", pesteree=pesteree)
+@app.route('/disappointment')
+def express_disappointment():
+    return render_template('disappointment.html', person=session.get('person'))
+
+@app.route('/details', methods=['GET', 'POST'])
+def ask_for_details():
+    return render_template('askfordetails.html', person=session.get('person'))
 
 if __name__ == '__main__':
     app.run()
