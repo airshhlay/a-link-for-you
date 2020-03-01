1a2416fc49c37b75fe4213a151fa8fd434f9c9a4
Ashleigh <ashleighaathar@gmail.com>
2020-03-02 00:27:46 +0800

    add more routes

--- app.py
+++ app.py
@@ -1,9 +1,13 @@
 import uuid
+from flask_debugtoolbar import DebugToolbarExtension
+from flask import Flask, render_template, request, redirect, url_for, session, flash
 
-from flask import Flask, render_template, request, redirect, url_for, session
+from image_generator import serve_pil_image, make_pil_image
 
 app = Flask(__name__, static_folder="/templates/static")
 app.secret_key = uuid.uuid4().hex
+app.debug = True
+toolbar = DebugToolbarExtension(app)
 
 @app.route('/', methods=['GET', 'POST'])
 def greetings():
@@ -24,7 +28,26 @@ def express_disappointment():
 
 @app.route('/details', methods=['GET', 'POST'])
 def ask_for_details():
+    if request.method == 'POST':
+        session['plan'] = request.form['plan']
+        session['date'] = request.form['date']
+        session['time'] = request.form['time']
+        session['location'] = request.form['location']
+        return redirect(url_for('detail_confirmation'))
     return render_template('askfordetails.html', person=session.get('person'))
+
+@app.route('/confirmation', methods=['GET', 'POST'])
+def detail_confirmation():
+    return render_template('confirmation.html')
+
+@app.route('/image.jpeg')
+def serve_img():
+    if all(val is not "" for val in session.values()):
+        img = make_pil_image(session.get('person'), session.get('plan'), session.get('date'), session.get('time'), session.get('location'))
+        return serve_pil_image(img)
+    else:
+        flash("You haven't entered everything!")
+        return redirect(url_for('ask_for_details'))
 
 if __name__ == '__main__':
     app.run()
